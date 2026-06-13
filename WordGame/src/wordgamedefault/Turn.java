0b0a63ad5cd935ae1aa1cee62035948030df8a6b
Evelin Garcia <eve2177109@maricopa.edu>
2026-06-13 14:08:51 -0700

    Added a scanner input in parameter and fixed player info code line.

--- WordGame/src/wordgamedefault/Turn.java
+++ WordGame/src/wordgamedefault/Turn.java
@@ -4,46 +4,43 @@ import java.util.Scanner;
 
 public class Turn {
 	
-	public Boolean takeTurn(Players player, Hosts host)
+	public Boolean takeTurn(Players player, Hosts host, Scanner input)
 	{
 		int guess;
 		int incAmount = 10;
 		int decAmount = 5;
 		
-		System.out.print(host.getFirstName() + " says 'Enter a number between 0 and 100: '");
+		System.out.println("----------------------------------");
+		System.out.print(host.getFirstName() + " says 'Enter a number between 0 and 100:' ");
 		
-		Scanner scnr = new Scanner(System.in);
-		while(!scnr.hasNextInt())
+		while(!input.hasNextInt())
 		{
-			System.out.print(host.getFirstName() + " says 'Please insert a number: '");
-			scnr.next();
+			System.out.print(host.getFirstName() + " says 'Please insert a number:' ");
+			input.next();
 		}
-		guess = scnr.nextInt();
-		scnr.close();
+		guess = input.nextInt();
 		
 		Numbers num = new Numbers();
 		
-		if (num.compareNumber(guess) == true)
+		if (num.compareNumber(guess))
 		{
-			num.compareNumber(guess);
-			
 			int newAmount = player.getCurrentMoney() + incAmount;
 			
 			player.setCurrentMoney(newAmount);
 			
-			player.toString();
+			System.out.println(player.toString());
+			System.out.println();
 			
 			return true;
 		}
 		else
 		{
-			num.compareNumber(guess);
-			
 			int newAmount = player.getCurrentMoney() - decAmount;
-			
+	
 			player.setCurrentMoney(newAmount);
 			
-			player.toString();
+			System.out.println(player.toString());
+			System.out.println();
 			
 			return false;
 		}
