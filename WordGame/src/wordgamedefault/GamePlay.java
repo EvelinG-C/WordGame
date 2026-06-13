20868354221c35de2feb1c8155c4d023967fe7d8
Evelin Garcia <eve2177109@maricopa.edu>
2026-06-13 14:17:38 -0700

    Updated according to Lesson 3 specifications.

--- WordGame/src/wordgamedefault/GamePlay.java
+++ WordGame/src/wordgamedefault/GamePlay.java
@@ -4,63 +4,71 @@ import java.util.Scanner;
 
 public class GamePlay {
 	
-	private static Person player;
+	private static Players player;
 
 	public static void main(String[] args) 
 	{
 		
-		System.out.print("Please enter your first name: ");
+		Hosts host = new Hosts("Garfield");
+		host.randomizeNum();
+		
+		System.out.println(host.getFirstName() + " says 'Welcome to the Random Word Game!'");
+		System.out.print(host.getFirstName() + " says 'Please enter your first name:' ");
 		Scanner scnr = new Scanner(System.in);
 		String firstName = scnr.next();
 		
-		System.out.print("Would you like to enter your last name? (yes or no) " );
+		System.out.print(host.getFirstName() + " says 'Would you like to enter your last name?' (yes or no) " );
 		String answer = scnr.next();
 		
 		String lastName;
 		if (answer.toLowerCase().equals("yes"))
 		{
-			System.out.print("What is your last name? ");
+			System.out.print(host.getFirstName() + " says 'What is your last name?' ");
 			lastName = scnr.next();
-			player = new Person(firstName, lastName);
+			player = new Players(firstName, lastName);
 		}
 		else
 		{
-			player = new Person(firstName);
+			player = new Players(firstName);
 		}
 		
-		Numbers number = new Numbers();
-		number.generateNumber();
+		Turn turn = new Turn();
 		
-		int guess;
-		
-		System.out.print("Enter your guess: ");
-		
-		while(!scnr.hasNextInt())
+		Boolean restartGame = true;
+		System.out.print(host.getFirstName() + " says 'Would you like to start the game?' (yes or no) ");
+		String decision = scnr.next();
+		System.out.println();
+		if (decision.toLowerCase().equals("yes"))
 		{
-			System.out.print("Please insert a number: ");
-			scnr.next();
+			restartGame = true;
 		}
-		guess = scnr.nextInt();
-		
-		boolean moreGuess = false; 
-		
-		while (moreGuess == false)
+		else
 		{
-			moreGuess = number.compareNumber(guess);
+			restartGame = false;
+		}
+		
+		while (restartGame == true)
+		{
 			
-			if (moreGuess == true)
+			while(turn.takeTurn(player, host, scnr) == false)
 			{
-				break;
+				continue;
 			}
 			
-			System.out.print("Enter your guess: ");
-			while(!scnr.hasNextInt())
+			System.out.print(host.getFirstName() + " says 'Would you like to restart the game?' ");
+			decision = scnr.next();
+			if (decision.toLowerCase().equals("yes"))
 			{
-				System.out.print("Please enter a valid number: ");
-				scnr.next();
+				host.randomizeNum();
+				restartGame = true;
 			}
-			guess = scnr.nextInt();
+			else
+			{
+				restartGame = false;
+			}
 		}
+		
+		System.out.println(host.getFirstName() + " says 'Thanks for playing!'");
 		
 		scnr.close();
 
