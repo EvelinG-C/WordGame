e35a0bb98a7fecad2484228dfb373cb477c67596
Evelin Garcia <eve2177109@maricopa.edu>
2026-06-19 14:00:01 -0700

    Added randomization of prize offered.

--- WordGame/src/wordgamedefault/Turn.java
+++ WordGame/src/wordgamedefault/Turn.java
@@ -1,16 +1,33 @@
 package wordgamedefault;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Turn {
 	
 	public Boolean takeTurn(Players player, Hosts host, Scanner input)
 	{
+		Numbers num = new Numbers();
+		Random randNum = new Random();
+		Money money = new Money();
+		Physical phys = new Physical();
+		
 		int guess;
-		int incAmount = 10;
-		int decAmount = 5;
+		int giftNumber;
 		
 		System.out.println("----------------------------------");
+		
+		giftNumber = randNum.nextInt(0,2);
+		
+		if (giftNumber == 0)
+		{
+			System.out.println(player.getFirstName() + ", 'You are trying to win money!'");
+		}
+		else
+		{
+			System.out.println(player.getFirstName() + ", 'You are trying to win a gift!'");
+		}
+		
 		System.out.print(host.getFirstName() + " says 'Enter a number between 0 and 100:' ");
 		
 		while(!input.hasNextInt())
@@ -18,31 +35,32 @@ public class Turn {
 			System.out.print(host.getFirstName() + " says 'Please insert a number:' ");
 			input.next();
 		}
+		
 		guess = input.nextInt();
 		
-		Numbers num = new Numbers();
+		Boolean trueOrFalse = num.compareNumber(guess);
 		
-		if (num.compareNumber(guess))
+		if (giftNumber == 0)
 		{
-			int newAmount = player.getCurrentMoney() + incAmount;
+			int newAmount = money.displayWinnings(player, trueOrFalse);
 			
-			player.setCurrentMoney(newAmount);
+			player.setCurrentMoney(player.getCurrentMoney() + newAmount);
 			
 			System.out.println(player.toString());
-			System.out.println();
 			
-			return true;
+			System.out.println();
 		}
-		else
+		else if (giftNumber == 1)
 		{
-			int newAmount = player.getCurrentMoney() - decAmount;
+			int newAmount = phys.displayWinnings(player, trueOrFalse);
 	
-			player.setCurrentMoney(newAmount);
+			player.setCurrentMoney(player.getCurrentMoney() + newAmount);
 			
 			System.out.println(player.toString());
-			System.out.println();
 			
-			return false;
+			System.out.println();
 		}
+		
+		return trueOrFalse;
 	}
 }
