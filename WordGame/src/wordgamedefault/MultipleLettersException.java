d2ece428a746e58438183afc01b1944915e0efd9
Evelin Garcia <eve2177109@maricopa.edu>
2026-06-25 13:33:54 -0700

    Initialized Custom MultipleLettersException

---
+++ WordGame/src/wordgamedefault/MultipleLettersException.java
@@ -0,0 +1,14 @@
+package wordgamedefault;
+
+public class MultipleLettersException extends Exception{
+	
+	public MultipleLettersException() {
+		
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return ("More than one letter was entered");
+	}
+}
