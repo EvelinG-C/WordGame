d88d1c0ae3f86e95e4ddedebdb0d165358645455
Evelin Garcia <eve2177109@maricopa.edu>
2026-08-01 13:40:23 -0700

    Updated so that physical prizes are given out properly.

--- WordGame/src/wordgamedefault/Turn.java
+++ WordGame/src/wordgamedefault/Turn.java
@@ -3,11 +3,18 @@ package wordgamedefault;
 import java.util.Random;
 
 public class Turn {
+	private Physical phys;
+	static boolean prizeGuessedCorrect;
+	
+	public Turn(Physical phys)
+	{
+		this.phys = phys;
+	}
+	
 	public Boolean takeTurn(Players player, Hosts host, String letter, Phrases phrase) 
 	{
 		Random randNum = new Random();
 		Money money = new Money();
-		Physical phys = new Physical();
 		
 		int giftNumber;
 		giftNumber = randNum.nextInt(0,2);
@@ -52,6 +59,7 @@ public class Turn {
 		}
 		
 		boolean guessedCorrect = true;
+		
 		if (ogPlayingPhrase.equals(phrase.getPlayingPhrase()))
 		{
 			guessedCorrect = false;
@@ -63,15 +71,25 @@ public class Turn {
 			int newAmount = money.displayWinnings(player, guessedCorrect);
 			player.setCurrentMoney(player.getCurrentMoney() + newAmount);
 			String playerInfo1 = player.toString() + money.getPlayerInfo();
+			prizeGuessedCorrect = false;
 			
 			GUI.dialogueArea.append(playerInfo1);
 		}
 		// 1 means gift
 		else if (giftNumber == 1)
 		{
-			int newAmount = phys.displayWinnings(player, wonGame);
+			int newAmount = phys.displayWinnings(player, guessedCorrect);
 			player.setCurrentMoney(player.getCurrentMoney() + newAmount);
-			String playerInfo1 = player.toString() + phys.getPlayerPrize();
+			String playerInfo1 = player.toString() + phys.getPrizeMessage();
+			
+			if (guessedCorrect)
+			{
+				prizeGuessedCorrect = true;
+			}
+			else
+			{
+				prizeGuessedCorrect = false;
+			}
 			
 			GUI.dialogueArea.append(playerInfo1);
 		}
